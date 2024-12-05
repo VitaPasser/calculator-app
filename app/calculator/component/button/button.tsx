94e622bf0e8e72d65e2af4d7950099fadd8b76d2
VitaPasser <vitapasser@gmail.com>
2024-12-05 16:54:46 +0000

    added history and some designe

--- app/calculator/component/button/button.tsx
+++ app/calculator/component/button/button.tsx
@@ -3,11 +3,12 @@ import React, { ReactNode } from 'react'
 type Props = {
     children: ReactNode;
     onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined 
+    className?: string | undefined
 }
 
-const Button = ({children, onClick}: Props) => {
+const Button = ({children, onClick, className}: Props) => {
   return (
-    <button className="m-5" onClick={onClick}>{children}</button>
+    <button className={"m-5 flex justify-center " + className} onClick={onClick}>{children}</button>
   )
 }
 
--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React, { useState } from 'react'
 import Button from './component/button/button'
+import { FaDeleteLeft } from "react-icons/fa6";
+
 
 type Props = {}
 type Expression = string[]
@@ -13,10 +15,15 @@ const Calculate = (expression: Expression) => {
         ['+', (x: number, y:number)=> x+y], 
         ['-', (x: number, y:number)=> x-y],
     ])
-    const operatorsKeys = operators.keys().toArray().map((value)=> "\\"+value).join('')
+    const operatorsKeys = operators.keys()
+                                   .toArray()
+                                   .map((value)=> "\\"+value)
+                                   .join('')
     
     let result = 0
-    const expression2 = expression[0] === '-' ? expression.join('') : ["+", ...expression].join('')
+    const expression2 = expression[0] === '-' 
+                        ? expression.join('') 
+                        : ["+", ...expression].join('')
     const getNumbers = expression2.split(new RegExp('['+operatorsKeys+'\(\)'+']', 'gui'))
                                   .filter((value)=> value !== "")
                                   .map((value) => parseFloat(value));
@@ -34,65 +41,85 @@ const Calculate = (expression: Expression) => {
 }
 
 const Calculator = (props: Props) => {
-    const [expression, setExpression] = useState<Expression>([])
-    const [result, setResult] = useState("0")
+    const [expression, setExpression] = useState<Expression>(["0"])
+    const [history, setHistory] = useState<Expression[]>([])
 
     return (
         <div className="flex flex-col justify-center m-10">
-            <h1>Calculator</h1>
-            <div>{expression.join("") + "=" + result} :output</div>
+            {history.filter((_, index)=> history.length - 5 < index).map((value, key)=>
+            <div className="text-3xl font-normal" key={key}>{value}</div>)}
+            <div className="text-6xl font-bold">{expression.join("")}</div>
             <div className="grid grid-cols-4">
                 
                 <Button onClick={() => {
-                    setExpression([])
-                    setResult("0")
+                    setExpression(["0"])
                 }}>AC</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => prevExpression.filter((_, index)=> index != (prevExpression.length-1)))
-                }}>del</Button>
+                    expression.length === 1 
+                    ? setExpression(["0"]) 
+                    : setExpression(prevExpression => prevExpression.filter((_, index)=> index != (prevExpression.length-1)))
+                }}><FaDeleteLeft /></Button>
                 <Button onClick={() => {
-                    setResult(prevResult => prevResult.includes("%") 
-                    ? "" + Calculate(expression)
-                    : parseFloat(prevResult)/100 + "%")
+                    setExpression(prevExpression => prevExpression[prevExpression.length - 1] === "%" 
+                    ? (parseFloat(prevExpression.filter((_, index) => prevExpression.length - 1 !== index).join(""))*100 + "").split("")
+                    : (parseFloat(prevExpression.join(""))/100 + "%").split(""))
                 }}>%</Button>
                 <Button onClick={() => {
                     setExpression(prevExpression => ['(', ...prevExpression, ')', "/"])
                 }}>/</Button>
                 
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "1"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["1"]) 
+                    : setExpression(prevExpression => [...prevExpression, "1"]);
                 }}>1</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "2"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["2"]) 
+                    : setExpression(prevExpression => [...prevExpression, "2"])
                 }}>2</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "3"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["3"]) 
+                    : setExpression(prevExpression => [...prevExpression, "3"])
                 }}>3</Button>
                 <Button onClick={() => {
                     setExpression(prevExpression => ['(', ...prevExpression, ')', "*"])
                 }}>*</Button>
                 
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "4"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["4"]) 
+                    : setExpression(prevExpression => [...prevExpression, "4"])
                 }}>4</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "5"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["5"]) 
+                    : setExpression(prevExpression => [...prevExpression, "5"])
                 }}>5</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "6"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["6"]) 
+                    : setExpression(prevExpression => [...prevExpression, "6"])
                 }}>6</Button>
                 <Button onClick={() => {
                     setExpression(prevExpression => [...prevExpression, "-"])
                 }}>-</Button>
                 
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "7"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["7"]) 
+                    : setExpression(prevExpression => [...prevExpression, "7"])
                 }}>7</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "8"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["8"]) 
+                    : setExpression(prevExpression => [...prevExpression, "8"])
                 }}>8</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "9"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["9"]) 
+                    : setExpression(prevExpression => [...prevExpression, "9"])
                 }}>9</Button>
                 <Button onClick={() => {
                     setExpression(prevExpression => [...prevExpression, "+"])
@@ -106,14 +133,17 @@ const Calculator = (props: Props) => {
                     )
                 }}>+/-</Button>
                 <Button onClick={() => {
-                    setExpression(prevExpression => [...prevExpression, "0"])
+                    expression.length === 1 && (expression[0] === "0") 
+                    ? setExpression(["0"]) 
+                    : setExpression(prevExpression => [...prevExpression, "0"])
                 }}>0</Button>
                 <Button onClick={() => {
                     setExpression(prevExpression => [...prevExpression, "."])
                 }}>.</Button>
-                <Button onClick={
-                    () => setResult("" + Calculate(expression))
-                }>=</Button>
+                <Button onClick={() => {
+                    setHistory(prevHistory => [...prevHistory, expression]);
+                    setExpression(Calculate(expression).toString().split(""));
+                }}>=</Button>
                 
             </div>
         </div>
